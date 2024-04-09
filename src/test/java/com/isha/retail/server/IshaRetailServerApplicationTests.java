805d44e3b2c326e5b2397fd61902d70cf7dddb80
Chetan Kukreja <kukreja.chetan@yahoo.com>
2024-04-09 17:01:30 +0530

    spring boot setup

---
+++ src/test/java/com/isha/retail/server/IshaRetailServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isha.retail.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IshaRetailServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
